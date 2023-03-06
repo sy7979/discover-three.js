b96f84b66568e8e3f87e1723ad6e37d17b0eefbe
윤서영 <sy7979@github.com>
2023-03-06 09:32:36 +0900

    📝 controls

---
+++ flamingo/src/World/systems/controls.js
@@ -0,0 +1,9 @@
+import { OrbitControls } from "../../../vendor/three/examples/jsm/controls/OrbitControls.js";
+
+function createControls(camera, canvas) {
+  const controls = new OrbitControls(camera, canvas)
+
+  return controls
+}
+asdfasd
+export { createControls }
